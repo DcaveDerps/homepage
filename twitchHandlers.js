9682d90a00aaea3519a58990c30be8e6660f2320
Derek H <djh118@pitt.edu>
2022-08-31 01:39:20 -0400

    added twitch live status

--- twitchHandlers.js
+++ twitchHandlers.js
@@ -16,3 +16,40 @@ async function getBroadcasterID(loginName, token, clientID) {
 
     return result;
 }
+
+twitchChannels = [
+    {
+        broadcaster_id: 79698024,
+        element_id: "twitchLiveStatus0"
+    },
+    {
+        broadcaster_id: 28219022,
+        element_id: "twitchLiveStatus1"
+    },
+    {
+        broadcaster_id: 25725272,
+        element_id: "twitchLiveStatus2"
+    }
+];
+
+async function getChannelLiveStatuses(token, clientID) {
+    for(i=0; i<twitchChannels.length; i++) {
+        let response = await fetch("https://api.twitch.tv/helix/streams?user_id=" + twitchChannels[i].broadcaster_id, {
+            headers: {
+                "Authorization": "Bearer " + token,
+                "Client-Id": clientID
+            },
+            method: "GET"
+        });
+
+        if (response.ok) {
+            responseJSON = await response.json();
+            if ( typeof responseJSON.data[0] !== 'undefined') {
+                document.getElementById(twitchChannels[i].element_id).innerHTML = "<strong>channel is live!</strong>";
+            }
+            else {
+                document.getElementById(twitchChannels[i].element_id).innerHTML = "channel is offline.";
+            }
+        }
+    }
+}
