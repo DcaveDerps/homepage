cc1ecac0808892f2b5f2b81047940f777b31229d
Derek H <djh118@pitt.edu>
2022-09-02 00:29:59 -0400

    cleaned up search bar

--- search.js
+++ search.js
@@ -1,12 +1,8 @@
 function search() {
-    console.log("you searched");
+
     let searchText = document.getElementById("searchBar").value.trim();
-    //console.log("search text:" + searchText);
     let formattedSearch = searchText.replace(/\s/g, "+");
-    //console.log("formatted search terms: " +  formattedSearch);
 
     window.location.href = "https://duckduckgo.com/?q=" + formattedSearch;
-
-    return false;
 
 }
