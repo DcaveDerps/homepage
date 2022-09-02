58cedb1e9297dd0009d92d75d098e785d014470d
Derek H <djh118@pitt.edu>
2022-09-02 02:44:06 -0400

    Added OpenMeteo weather integration

--- weather.js
+++ weather.js
@@ -1,4 +1,5 @@
 // Weather.gov API
+const TEMP_UNIT = " °F"
 
 function formatForecast(forecastStr) {
     let lines = forecastStr.split(". ");
@@ -20,7 +21,7 @@ async function getWeatherGovForecast() {
         let temp0 = response.properties.periods[0].temperature;
         let forecast0 = response.properties.periods[0].detailedForecast;
         console.log("Temp is " + temp0 + " degrees Fahrenheit");
-        document.getElementById("weatherGovTemp").textContent = "Temp is " + temp0 + " degrees Fahrenheit.";
+        document.getElementById("weatherGovTemp").textContent = "Temp is " + temp0 + TEMP_UNIT;
         document.getElementById("weatherGovForecast").innerHTML = formatForecast(forecast0);
         document.getElementById("forecastImg").src = response.properties.periods[0].icon;
     }
@@ -29,16 +30,54 @@ async function getWeatherGovForecast() {
 // Open Weather API
 
 function initOpenMeteo() {
-    
+    let weatherPaneHTML = document.getElementById("weatherPane");
+
+    weatherPaneHTML.innerHTML = weatherPaneHTML.innerHTML + "<div id=\"openMeteo\">OpenMeteo Report<br></div>";
+
 }
 
 async function getOpenMeteoForecast() {
-    let openMeteoData = await fetch("https://api.open-meteo.com/v1/forecast?latitude=40.44&longitude=-79.95&hourly=temperature_2m,apparent_temperature,rain,showers,snowfall,snow_depth&daily=temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,sunrise,sunset&temperature_unit=fahrenheit&timeformat=unixtime&timezone=America%2FNew_York");
+    let openMeteoData = await fetch("https://api.open-meteo.com/v1/forecast?latitude=40.44&longitude=-79.95&hourly=temperature_2m,apparent_temperature,rain,showers,snowfall,snow_depth&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timezone=America%2FNew_York");
+
+    let hour = new Date().getHours();
 
     if (openMeteoData.ok) {
-        let openMetoResponse = await openMeteoData.json();
+        let openMeteoResponse = await openMeteoData.json();
+        let openMeteoPane = document.getElementById("openMeteo");
+
+        let hourlyData = openMeteoResponse.hourly;
+        let dailyData = openMeteoResponse.daily;
+
+        openMeteoPane.innerHTML = openMeteoPane.innerHTML + "<div>Temperature: " + hourlyData.temperature_2m[0] + TEMP_UNIT + "</div>" +
+        "<div>Feels like: " + hourlyData.apparent_temperature[hour] + TEMP_UNIT + "</div>" + 
+        "<div> High: " + dailyData.temperature_2m_max[0] + TEMP_UNIT + "</div>" +
+        "<div> Low: " + dailyData.temperature_2m_min[0] + TEMP_UNIT + "</div>";
+
+        if (hourlyData.rain[hour] > 0) {
+            openMeteoPane.innerHTML = openMeteoData.innerHTML + "<div>Rainfall: " + hourlyData.rain[hour] + " inches</div>";
+        }
+        if (hourlyData.snowfall[hour] > 0) {
+            openMeteoPane.innerHTML = openMeteoData.innerHTML + "<div>Snowfall: " + hourlyData.snowfall[hour] + " cm</div>";
+        }
+        if (hourlyData.snow_depth[hour] > 0) {
+            openMeteoPane.innerHTML = openMeteoData.innerHTML + "<div>Snowfall: " + hourlyData.snow_depth[hour] + " m</div>";
+        }
+        if (hourlyData.showers[hour] > 0) {
+            openMeteoPane.innerHTML = openMeteoData.innerHTML + "<div>Showers: " + hourlyData.showers[hour] + " inches</div>";
+        }
+
+        openMeteoPane.innerHTML = openMeteoPane.innerHTML + "<div>Sunrise: " + new Date(dailyData.sunrise[0]).toLocaleTimeString() + "</div>" +
+        "<div>Sunset: " + new Date(dailyData.sunset[0]).toLocaleTimeString() + "</div>";
+
+    }
+    else {
+        openMeteoPane.innerHTML = openMeteoPane.innerHTML + "<div><strong>Error retrieving data.</strong></div>";
     }
 
+}
+
+function initWeatherPane() {
+    initOpenMeteo();
 }
 
 async function getWeatherForecasts() {
