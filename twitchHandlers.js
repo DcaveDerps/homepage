7faf9fcbc0eafeb2940ce18369a22d2fd307a344
Derek H <djh118@pitt.edu>
2022-09-05 02:30:52 -0400

    Twitch OAUTH token is now cached locally, speeding up page loading

--- twitchHandlers.js
+++ twitchHandlers.js
@@ -51,28 +51,36 @@ twitchChannels = [
     }
 ];
 
-async function initTwitchPane() {
+async function initTwitchPane(noRetry) {
     let twitchPane = document.getElementById("twitchLivePane");
     // liveChannels and offlineChannels will contain the indeces of the
     // twitch channels in the array twitchChannels based on their status
     let liveChannels = [];
     let offlineChannels = [];
 
-    // init the channels
-    for (i=0;i<twitchChannels.length; i++) {
-        let channel = twitchChannels[i];
-        twitchPane.innerHTML = twitchPane.innerHTML + buildChannelBoxHTML(channel.login, channel.profilePic, CHECKING_IMG_PATH);
+    // init the channel boxes so there's no pop-in
+    // check noRetry to ensure channel boxes aren't duplicated due to recursive call
+    if (noRetry !== true) {
+        for (i=0;i<twitchChannels.length; i++) {
+            let channel = twitchChannels[i];
+            twitchPane.innerHTML = twitchPane.innerHTML + buildChannelBoxHTML(channel.login, channel.profilePic, CHECKING_IMG_PATH);
+        }
     }
 
     let token = await getOAUTHToken();
 
     if (token !== undefined) {
         await getChannelLiveStatuses(token, getClientID(), liveChannels, offlineChannels);
-        console.log("After call to getChannelLiveStatuses: liveChannels.length: " + liveChannels.length + " | offlineChannels.length: " + offlineChannels.length);
+    }
+    else if (noRetry !== true) {
+        // Error retrieving data from Twitch, mark all channels as such
+        console.log("Error: OAUTH token may be uninitialized. Resetting and trying again...");
+        await getOAUTHToken(true);
+        initTwitchPane(true);
+        return;
     }
     else {
-        // Error retrieving data from Twitch, mark all channels as such
-        console.log("Error receiving OAUTH token from Twitch");
+        console.log("protectedScripts.js is missing!");
         return;
     }
 
@@ -92,10 +100,6 @@ async function initTwitchPane() {
 
 }
 
-// TODO:
-// Sort the streams by live and offline and error, with the order being live->offline->error, with live being topmost, then offline, then error
-// Might be possible to have the icons appear from off screen?
-
 function buildChannelBoxHTML(login, profilePic, statusImage) {
     return "<div class=\"twitchChannelBox\" id=\"twitchChannel_" + login + "\">" +
     "<a href=https://www.twitch.tv/" + login + " class=\"imageLink\"><img class=\"twitchIcon\" src=" + profilePic + " >" + 
@@ -104,7 +108,7 @@ function buildChannelBoxHTML(login, profilePic, statusImage) {
     "</div>";
 }
 
-async function getChannelLiveStatuses(token, clientID, liveChannels, offlineChannels) {
+async function getChannelLiveStatuses(token, clientID, liveChannels, offlineChannels, noRetry) {
     let requestURL = "https://api.twitch.tv/helix/streams?";
 
     for (i = 0; i < twitchChannels.length; i++) {
@@ -144,65 +148,27 @@ async function getChannelLiveStatuses(token, clientID, liveChannels, offlineChan
                 offlineChannels[offlineChannels.length] = i;
             }
         }
-        console.log("After loops in getChannelLiveStatuses: liveChannels.length: " + liveChannels.length + " | offlineChannels.length: " + offlineChannels.length);
+    }
+    else if (noRetry !== true) {
+        console.log("Error: " + response.statusText);
+        console.log("Resetting OAUTH and trying again...");
+        // update OAUTH token and try again
+        token = await getOAUTHToken(true);
+        await getChannelLiveStatuses(token, clientID, liveChannels, offlineChannels, true);
     }
 }
 
-/*
-async function getChannelLiveStatuses(token, clientID) {
-    let requestURL = "https://api.twitch.tv/helix/streams?";
-
-    for (i = 0; i < twitchChannels.length; i++) {
-        requestURL = requestURL + "user_login=" + twitchChannels[i].login;
-        if (i + 1 !== twitchChannels.length) {
-            requestURL = requestURL + "&";
-        }
-    }
-
-    //console.log(requestURL);
-
-    let response = await fetch(requestURL, {
-            headers: {
-                "Authorization": "Bearer " + token,
-                "Client-Id": clientID
-            },
-            method: "GET"
-        });
-    
-    if (response.ok) {
-        responseJSON = await response.json();
-
-        // assume all are offline
-        for (i = 0; i < twitchChannels.length; i++) {
-                document.getElementById("twitchLiveStatus_" + twitchChannels[i].login).src = "staticFiles\\offlineIcon.png";
-        }
-
-
-        // check each result in the data array
-        // if response.data is empty, no one is online, and they're already marked as such
-
-        // if response.data is not empty, check each result and mark each channel live accordingly
-
-        // streams are returned sorted by viewcount, can't rely on consistent order
-        for (i = 0; i < responseJSON.data.length; i++){
-            for (j = 0; j < twitchChannels.length; j++) {
-                if (responseJSON.data[i].user_login === twitchChannels[j].login) {
-                    document.getElementById("twitchLiveStatus_" + twitchChannels[j].login).src = "staticFiles\\liveIcon.png";
-                    break;
-                }
-            }
-        }
-
-
-    }
-}
-*/
-
-async function getOAUTHToken() {
+async function getOAUTHToken(forceReset) {
     // check if the necesary js file exists
-    if (typeof refreshOAUTH === "function") {
-        let token = await refreshOAUTH();
-        return token;
+    if (typeof resetOAUTH === "function") {
+        if (forceReset) {
+            let token = await resetOAUTH();
+            localStorage["OAUTHToken"] = token;
+            return token;
+        }
+        else {
+            return localStorage["OAUTHToken"];
+        }
         //let clientID = await getClientID();
         //await getChannelLiveStatuses(token, clientID);
 
@@ -210,7 +176,7 @@ async function getOAUTHToken() {
         //console.log("id is: " + broadcasterID);
     }
     else {
-        console.log("necessary .js file missing");
+        console.log("necessary .js file is missing");
         return undefined;
     }
 }
