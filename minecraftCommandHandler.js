a40fdbcbd0031aa0fe7693b37acff86d81f28af0
Derek H <djh118@pitt.edu>
2022-09-24 01:50:36 -0400

    Styled command submit button. Added some input validation

--- minecraftCommandHandler.js
+++ minecraftCommandHandler.js
@@ -15,49 +15,40 @@ function initCommandsPane() {
 function absToRelCoords(){
     let command = document.getElementById("commandInputBox").value;
 
-    // test command
-    //command = "/fill -20 2 5 50 4 5 minecraft:iron_block";
-
-    console.log("original: " + command);
-
     let coordTuplePattern = new RegExp(/-?\d+\.?\d*\s-?\d+\.?\d*\s-?\d+\.?\d*/, 'g')
-    let coordTuplePatternOnce = new RegExp(/-?\d+\.?\d*\s-?\d+\.?\d*\s-?\d+\.?\d*/);
     // find all of the coord tuples
     let tuples = command.matchAll(coordTuplePattern);
+
+    let curMatch = tuples.next();
+    if (curMatch.done) { return; }
+
     command = command.replace(coordTuplePattern, "tup");
 
-    console.log("found matches:");
-
     // convert the tuples to relative coords
-    let curMatch = tuples.next();
     while (!curMatch.done) {
-        console.log(curMatch.value);
+
         let relTuple = absTupleToRelCoords(curMatch.value[0]);
-        console.log(relTuple);
-        if (typeof(relTuple) === typeof(undefined)) {
+
+        if (relTuple === undefined) {
             return;
         }
         else {
-            console.log("before replace: " + command);
             command = command.replace("tup", relTuple);
-            console.log("after replace: " + command);
         }
         curMatch = tuples.next();
     }
-
-    console.log(command);
 
     appendOutput(command);
 
 }
 
 function absTupleToRelCoords(tupleString) {
-    let blockX = Number(document.getElementById("xCoord").value);
-    let blockY = Number(document.getElementById("yCoord").value);
-    let blockZ = Number(document.getElementById("zCoord").value);
+    let blockX = document.getElementById("xCoord").value;
+    let blockY = document.getElementById("yCoord").value;
+    let blockZ = document.getElementById("zCoord").value;
 
-    // ADD ERROR CHECKING
-    if (isNaN(blockX) || isNaN(blockY) || isNaN(blockZ)) {
+    // ADD ERROR NOTIFICATION
+    if ((isNaN(blockX) || blockX === "") || (isNaN(blockY) || blockY === "") || (isNaN(blockZ) || blockZ === "")) {
         return undefined;
     }
 
@@ -65,9 +56,8 @@ function absTupleToRelCoords(tupleString) {
     for (let i = 0; i < coords.length; i++) {
         coords[i] = Number(coords[i]);
     }
-    console.log(coords);
 
-    return "~" + (coords[0] - blockX) + " ~" + (coords[1] - blockY) + " ~" + (coords[2] - blockZ);
+    return "~" + (coords[0] - Number(blockX)) + " ~" + (coords[1] - Number(blockY)) + " ~" + (coords[2] - Number(blockZ));
 
 }
 
